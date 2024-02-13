dd5e1bdc30d146e4a3b1d9b29547ce8f77925571
siwinile <141214474+siwinile@users.noreply.github.com>
2024-02-13 09:28:49 +0200

    picture

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,35 +1,41 @@
 import React from "react";
 import "../style/Projects.css";
-import Projects from "../images/images.jpg"
+import Images from "../images/images.jpg"
 
 
-const Slideshow = ({ images }) => {
-  const [currentSlide, setCurrentSlide] = React.useState(0);
+// Projects.js
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
 
-  const handleNextSlide = () => {
-    setCurrentSlide((currentSlide + 1) % images.length);
-  };
-
-  const handlePrevSlide = () => {
-    setCurrentSlide(
-      (currentSlide - 1 + images.length) % images.length
-    );
-  };
+const Projects = () => {
+  const projects = [
+    {
+      title: 'Project  1',
+      imageUrl: 'path-to-image-1.jpg',
+      description: 'Description for Project  1'
+    },
+    {
+      title: 'Project  2',
+      imageUrl: 'path-to-image-2.jpg',
+      description: 'Description for Project  2'
+    },
+    // ... more projects
+  ];
 
   return (
-    <div className="slideshow">
-      <div className="slideshow-slide">
-        <img src={images[currentSlide].image} alt={images[currentSlide].caption} />
-        <div className="slideshow-caption">{images[currentSlide].caption}</div>
-      </div>
-      <button className="slideshow-prev" onClick={handlePrevSlide}>
-        ❮
-      </button>
-      <button className="slideshow-next" onClick={handleNextSlide}>
-        ❯
-      </button>
+    <div className="projects-section">
+      <h2>Projects</h2>
+      <Carousel>
+        {projects.map((project, index) => (
+          <div key={index}>
+            <img src={project.imageUrl} alt={project.title} />
+            <p className="legend">{project.description}</p>
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
 
-export default Slideshow;
+export default Projects;
