bd04ba58f486848f7c1a0b5aaa78dce3bbc7b984
siwinile <141214474+siwinile@users.noreply.github.com>
2024-02-16 10:34:23 +0200

    Testimonials

--- portfolio/src/pages/Testimonials.jsx
+++ portfolio/src/pages/Testimonials.jsx
@@ -3,8 +3,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import "../style/Testimonial.css";
 
 import Chuma from "../images/Chuma.png";
-import Lihle from "../images/Lihle.jpg";
-import Sbo from "../images/Sbo.jpg";
+import Lihle from "../images/Lihle.png";
+import Sbo from "../images/Sbo.png";
 
 function Projects() {
     return (
@@ -15,42 +15,43 @@ function Projects() {
       <img className="Chuma" src={Chuma} alt="First slide" />
       <div className="additional-content">
         {/* Additional content here */}
-        <h3>Additional Information</h3>
-        <p>Some additional description or details.</p>
+        <h3>Chuma Raxothi</h3>
+        <p>Software Developer.</p>
       </div>
     </div>
     <Carousel.Caption>
       <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <p>I have no doubt that Siwinile would be a tremendous asset to any project or team, I look forward to the opportunity to work with her again in the future.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
-        <Carousel.Item>
-          <img className="Lihle"src={Lihle} alt="Second slide" />
-          <div className="additional-content">
-        {/* Additional content here */}
-        <h3>Additional Information</h3>
-        <p>Some additional description or details.</p>
-      </div>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="Sbo" src={Sbo} alt="Third slide" />
-          <div className="additional-content">
-        {/* Additional content here */}
-        <h3>Additional Information</h3>
-        <p>Some additional description or details.</p>
-      </div>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+  <Carousel.Item>
+  <div className="image-with-box">
+    <img className="Lihle" src={Lihle} alt="Second slide" />
+    <div className="additional-content">
+      <h3>Additional Information</h3>
+      <p>Software Developer.</p>
+    </div>
+  </div>
+  <Carousel.Caption>
+    <h3>Second slide label</h3>
+    <p>What I appreciate about Siwinile is her ability to finish a tasks in time.This is not only show cases her talent but also her commitment to going above and beyond her expectations.</p>
+  </Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+  <div className="d-flex justify-content-between align-items-center">
+  <img className="Sbo smaller-image" src={Sbo} alt="Second slide" />
+    <div className="additional-content">
+      <h3>Additional Information</h3>
+      <p>Some additional description or details.</p>
+    </div>
+  </div>
+  <Carousel.Caption>
+    <h3>Second slide label</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </Carousel.Caption>
+</Carousel.Item>
+
       </Carousel>
     );
 }
