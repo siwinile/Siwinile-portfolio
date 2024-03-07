282de80e0ef8a38df4d2e6b0ec298a87fdf37230
siwinile <141214474+siwinile@users.noreply.github.com>
2024-03-07 14:54:32 +0200

    skillsection

--- portfolio/src/pages/SkillSection.jsx
+++ portfolio/src/pages/SkillSection.jsx
@@ -3,53 +3,76 @@ import "../style/SkillSection.css";
 
 function Canvas() {
   return (
-    <section id="Canvas" className="canvas--section">
-      <div className="heading">
-        <h1>Skills</h1>
-      </div>
-    <div className="canvas"> 
-      <div className="canvas__content">
-        <div className="canvas__section">
-          <h2>Front-end development</h2>
-          <p>
-            My canvas in the world of web development is HTML and CSS. I create clear, semantic HTML structures and give them a modern, responsive style makeover.
-          </p>
-          <p>
-            JavaScript is the foundation of my work. I use JavaScript to create user-friendly and entertaining online applications.
-          </p>
-          <h2>Back-end development</h2>
-          <p>
-            Java is my dependable travel partner when I venture to the server-side. In order to make sure that the logic behind my applications is just as solid as the user interface, Python empowers me to solve complex problems with simplicity.
-          </p>
+    <section class="skill-section" id="skill">
+        <div class="container">
+            <h2 class="section-title" data-enllax-ratio="0.5" data-enllax-type="foreground" data-enllax-direction="horizontal">My Skills</h2>
+            
+            <div class="skill-wrapper" id="me-working-skill">
+                <span class="vertical-line"></span>
+                <div class="single-skill animate-me">
+                    <span class="circle"></span>
+                    <span class="rectengle"></span>
+                    <h3><span class="title">html</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="single-skill animate-me">
+                    <span class="circle"></span>
+                    <span class="rectengle"></span>
+                    <h3><span class="title">css</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptate quod odit excepturi nisi!</p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">oracle Apex</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptate quod odit excepturi nisi! Nostrum earum quia quaerat modi molestiae.</p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">JavaScript</span> <span class="line"></span></h3>
+                    <p>JavaScript is my expertise, especially in frontend development. I've created numerous interactive and dynamic web applications using JavaScript, including modern frameworks like React.js </p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">caspio</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptate quod odit excepturi nisi! </p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">python</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">firbase</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">C#</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">react.js</span> <span class="line"></span></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="single-skill animate-me">
+                   <span class="circle"></span>
+                   <span class="rectengle"></span>
+                    <h3><span class="title">java</span> <span class="line"></span></h3>
+                    <p>Java is my go-to language for backend development and Android apps.</p>
+                </div>
+            </div>
         </div>
-        <div className="canvas__section">
-          <h2>Skills</h2>
-          <ul>
-            <li>HTML</li>
-            <div className="progress-bar">
-              <div className="progress" style={{ width: "60%" }}>80%</div>
-            </div>
-            <li>CSS</li>
-            <div className="progress-bar">
-              <div className="progress" style={{ width: "70%" }}>90%</div>
-            </div>
-            <li>JavaScript</li>
-            <div className="progress-bar">
-              <div className="progress" style={{ width: "80%" }}>60%</div>
-            </div>
-            <li>Python</li>
-            <div className="progress-bar">
-              <div className="progress" style={{ width: "75%" }}>75%</div>
-            </div>
-            <li>Java</li>
-            <div className="progress-bar">
-              <div className="progress" style={{ width: "65%" }}>65%</div>
-            </div>
-          </ul>
-        </div>
-      </div>
-    </div>
     </section>
+
   );
 }
 
