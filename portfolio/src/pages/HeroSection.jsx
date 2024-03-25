2b713722390fc4f812d69176488f2dd83ca14a43
siwinile <141214474+siwinile@users.noreply.github.com>
2024-03-25 11:29:23 +0200

    herosection

--- portfolio/src/pages/HeroSection.jsx
+++ portfolio/src/pages/HeroSection.jsx
@@ -41,19 +41,20 @@ const Portfolio = () => {
           <nav>
             <input type="checkbox" id="menu-toggle" checked={menuOpen} onChange={toggleMenu} />
             <label htmlFor="menu-toggle" className="hamburger">&#9776;</label>
+            <img src={image} alt="logo" className="logo" />
             <ul className={`menu ${menuOpen ? 'active' : ''}`}>
               <li><a href="#hero" data-after="Home">Home</a></li>
-              <li><a href="#AboutMe" data-after="Service">Services</a></li>
-              <li><a href="#skills" data-after="skills">Projects</a></li>
-              <li><a href="#Testimonials" data-after="Testimonials">About</a></li>
-              <li><a href="#contact" data-after="Contact">Contact</a></li>
+              <li><a href="#AboutMe" data-after="About">About</a></li>
+              <li><a href="#skills" data-after="skills">Skills</a></li>
+              <li><a href="#Testimonials" data-after="Testimonials">Testimonials</a></li>
+              <li><a href="#Contact" data-after="Contact">Contact</a></li>
             </ul>
           </nav>
           
           <div className="content">
             <div className="div-text">
-              <h1>Hi, I'am Siwinile</h1>
-              <h4>UX / UI Designer</h4>
+              <h1>Hi, I'm Siwinile</h1>
+              <h4>Software Developer/UX / UI Designer</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Labore reprehenderit veritatis aperiam sed alias quos. Aliquid vero autem sapiente aspernatur?</p>
                 <DownloadCVButton /> 
@@ -63,10 +64,12 @@ const Portfolio = () => {
             </div>
           </div>
           <div className="soc-link">
-            <a href=""><FontAwesomeIcon icon={faFacebookF} /></a>
-            <a href=""><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href=""><FontAwesomeIcon icon={faInstagram} /></a>
-          </div>
+                    <a href="https://www.instagram.com/winnieeuchar/"><i class="fa-brands fa-square-facebook"></i></a>
+                    <a href="https://www.facebook.com/umbali.author"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/siwinile-mdani-a78b58253/"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-square-whatsapp"></i></a>
+                </div>
         </div>
       </div>
     </header>
